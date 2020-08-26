de6d79de5dfe57274502f464997beda0ef8ee0b1
Teemu <teemu_typpi@hotmail.com>
2020-08-26 11:02:51 +0200

    added frontend build to same dir

---
+++ build/precache-manifest.2cb161f241eb213d33008cce30b088de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5389084abb3d69526c6f228928eb160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86c95834b03e662acde3",
+    "url": "/static/css/main.cee87863.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c95834b03e662acde3",
+    "url": "/static/js/main.55b91def.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
